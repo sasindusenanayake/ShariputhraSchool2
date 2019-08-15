777283d93a2f1dcfe20fc2ee4e6be8ed4d2ea338
sasindu  senanayake <senanayakesasindu@gmail.com>
2019-08-16 00:39:04 +0530

    add gitignore

--- src/com/shariputhra/batabase/connectionManager.java
+++ src/com/shariputhra/batabase/connectionManager.java
@@ -2,7 +2,6 @@ package com.shariputhra.batabase;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class connectionManager {
