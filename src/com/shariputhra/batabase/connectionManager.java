b3feebfc38761ed9abfd8ea7d341d00aba3005fd
sasindu  senanayake <senanayakesasindu@gmail.com>
2019-08-16 00:23:04 +0530

    initalize project

---
+++ src/com/shariputhra/batabase/connectionManager.java
@@ -0,0 +1,31 @@
+package com.shariputhra.batabase;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class connectionManager {
+
+	private Connection con = null;
+	private final String CONNECTIONSTRING = "jdbc:mysql://localhost:3309/school_management_system";
+	private final String USERNAME = "root";
+	private final String PASSWORD = "";
+	
+	public Connection getCon() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			try {
+
+				con = DriverManager.getConnection(CONNECTIONSTRING,USERNAME,PASSWORD);
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+}
